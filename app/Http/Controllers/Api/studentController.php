05c6e52640100c903a4b4871c90288e10605995f
Luis Medina <lmmedina1205@gmail.com>
2026-01-03 22:09:41 -0500

    Agregar controlador y modelo de estudiante, rutas API y configuración de la api con MYSQL

---
+++ app/Http/Controllers/Api/studentController.php
@@ -0,0 +1,207 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Student;
+use Illuminate\Support\Facades\Validator;
+
+class studentController extends Controller
+{
+    public function index()
+    {
+        $students = Student::all();
+
+        // if ($students->isEmpty()) {
+        //     $data = [
+        //         'message' => 'No se encontraron estudiantes.',
+        //         'status' => 200
+        //     ];
+        //     return response()->json($data, 404);
+        // }
+
+        $data = [
+            'students' => $students,
+            'status' => 200
+        ];
+
+        return response()->json($data, 200);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+            'email' => 'required|email|unique:student',
+            'phone' => 'required|digits:10',
+            'language' => 'required|in:English,Spanish,French'
+        ]);
+
+        if ($validator->fails()) {
+            $data = [
+                'message' => 'Error de validación de los datos.',
+                'errors' => $validator->errors(),
+                'status' => 400
+            ];
+            return response()->json($data, 400);
+        }
+
+        $student = Student::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'phone' => $request->input('phone'),
+            'language' => $request->input('language')
+        ]);
+
+        if (!$student) {
+            $data = [
+                'message' => 'Error al crear el estudiante.',
+                'status' => 500
+            ];
+            return response()->json($data, 500);
+        }
+
+        $data = [
+            'message' => 'Estudiante creado exitosamente.',
+            'student' => $student,
+            'status' => 201
+        ];
+
+        return response()->json($data, 201);
+    }
+
+    public function show($id)
+    {
+        $student = Student::find($id);
+        if (!$student) {
+            $data = [
+                'message' => 'Estudiante no encontrado.',
+                'status' => 404
+            ];
+            return response()->json($data, 404);
+        }
+
+        $data = [
+            'student' => $student,
+            'status' => 200
+        ];
+
+        return response()->json($data, 200);
+    }
+
+    public function destroy($id)
+    {
+        $student = Student::find($id);
+        if (!$student) {
+            $data = [
+                'message' => 'Estudiante no encontrado.',
+                'status' => 400
+            ];
+            return response()->json($data, 400);
+        }
+
+        $student->delete();
+
+        $data = [
+            'message' => 'Estudiante eliminado.',
+            'status' => 200
+        ];
+
+        return response()->json($data, 200);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $student = Student::find($id);
+        if (!$student) {
+            $data = [
+                'message' => 'Estudiante no encontrado.',
+                'status' => 404
+            ];
+            return response()->json($data, 404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+            'email' => 'required|email|unique:student',
+            'phone' => 'required|digits:10',
+            'language' => 'required|in:English,Spanish,French'
+        ]);
+
+        if ($validator->fails()) {
+            $data = [
+                'message' => 'Error de validación de los datos.',
+                'errors' => $validator->errors(),
+                'status' => 400
+            ];
+            return response()->json($data, 400);
+        }
+
+        $student->name = $request->input('name');
+        $student->email = $request->input('email');
+        $student->phone = $request->input('phone');
+        $student->language = $request->input('language');
+
+        $student->save();
+
+        $data = [
+            'message' => 'Estudiante actualizado exitosamente.',
+            'student' => $student,
+            'status' => 200
+        ];
+
+        return response()->json($data, 200);
+    }
+
+    public function updatePartial(Request $request, $id)
+    {
+        $student = Student::find($id);
+        if (!$student) {
+            $data = [
+                'message' => 'Estudiante no encontrado.',
+                'status' => 404
+            ];
+            return response()->json($data, 404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'max:255',
+            'email' => 'email|unique:student',
+            'phone' => 'digits:10',
+            'language' => 'in:English,Spanish,French'
+        ]);
+
+        if ($validator->fails()) {
+            $data = [
+                'message' => 'Error de validación de los datos.',
+                'errors' => $validator->errors(),
+                'status' => 400
+            ];
+            return response()->json($data, 400);
+        }
+
+        if ($request->has('name')) {
+            $student->name = $request->input('name');
+        }
+        if ($request->has('email')) {
+            $student->email = $request->input('email');
+        }
+        if ($request->has('phone')) {
+            $student->phone = $request->input('phone');
+        }
+        if ($request->has('language')) {
+            $student->language = $request->input('language');
+        }
+
+        $student->save();
+
+        $data = [
+            'message' => 'Estudiante actualizado exitosamente.',
+            'student' => $student,
+            'status' => 200
+        ];
+
+        return response()->json($data, 200);
+    }
+}
