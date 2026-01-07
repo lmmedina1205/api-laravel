3090abedc2c6f5f54ebc655628163fb0a37e5944
Luis Medina <lmmedina1205@gmail.com>
2026-01-06 23:47:50 -0500

    Add SoftDeletes trait to Student model and create migration for soft deletes

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -3,11 +3,13 @@
 namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Student extends Model
 {
     use hasFactory;
 
+    use SoftDeletes;
     protected $table = 'student';
 
     protected $fillable = [
